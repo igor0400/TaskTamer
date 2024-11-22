e1a9b0531f5cc875d18da3718966fb04b99f5ed2
Igor0400 <ulanov.igor0400@gmail.com>
2024-11-22 23:40:25 +0300

    start chats

--- src/chats/chats.service.ts
+++ src/chats/chats.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { getCtxData } from 'src/libs/common';
+import { Context } from 'telegraf';
 
 @Injectable()
-export class ChatsService {}
+export class ChatsService {
+  async onBotAddedToChat(ctx: Context) {
+    const { message } = getCtxData(ctx);
+    const { chat, new_chat_member } = message;
+
+    if (
+      (new_chat_member &&
+        new_chat_member.username === process.env.BOT_USERNAME) ||
+      !new_chat_member
+    ) {
+      ctx.reply('Hello!');
+    }
+
+    const admins = await ctx.telegram.getChatAdministrators(chat.id);
+
+    console.log(admins);
+  }
+}
--- src/chats/chats.update.ts
+++ src/chats/chats.update.ts
@@ -1,4 +1,13 @@
-import { Update } from 'nestjs-telegraf';
+import { On, Update } from 'nestjs-telegraf';
+import { Context } from 'telegraf';
+import { ChatsService } from './chats.service';
 
 @Update()
-export class ChatsUpdate {}
+export class ChatsUpdate {
+  constructor(private readonly chatsService: ChatsService) {}
+
+  @On(['new_chat_members', 'group_chat_created'])
+  async onAddToChat(ctx: Context) {
+    await this.chatsService.onBotAddedToChat(ctx);
+  }
+}
